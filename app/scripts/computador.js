28e43376d4baf2bed35bd980d5dfad2b2f716cb6
Igor Gonçalves <igoncalves@localhost.localdomain>
2017-03-28 02:22:00 -0300

    init

---
+++ app/scripts/computador.js
@@ -0,0 +1,78 @@
+function Computador(){
+  this.memoria = [];
+  this.processador = new Processador();
+  this.carregarMemoria = function (memoria){
+    this.memoria = memoria;
+  }
+}
+
+function Processador(){
+  this.cp = 0; // contador de programas
+  this.ula = new Ula();
+  this.uc = new UC();
+  this.dc = new Decodificador();
+  this.start = function(memoria){
+    do {
+
+      this.cp = this.uc.control(this.dc.decodificar(memoria[this.cp]), this.cp, this.ula);
+
+    } while (memoria.length > this.cp);
+  }
+}
+
+function Decodificador(){
+  var maskComando = 0b11110000000000000000000000000000;
+  var maskValor1  = 0b00001111111111111100000000000000;
+  var maskValor2  = 0b00000000000000000011111111111111;
+  var instrucao = [];
+  this.decodificar = function(palavra){
+    instrucao = [];
+    palavra = parseInt(palavra, 2);
+    instrucao.push((palavra & maskComando) >> 28);
+    instrucao.push((palavra & maskValor1) >> 14);
+    instrucao.push(palavra & maskValor2);
+    return instrucao;
+  }
+}
+
+
+function UC(){
+  this.control = function(command, cp, ula){
+    if (command[0] == 0b100) {
+      return command[2];
+    }
+    console.log("command: "+ command[0].toString(2)+", " +
+                             command[1].toString(2)+", " +
+                             command[2].toString(2)+
+                             " Resultado: "+ula.calcule(command).toString(2));
+    return cp + 1;
+  }
+}
+
+
+function Ula(){
+  this.calcule = function(command){
+    switch (command[0]) {
+      case 1:
+        return this.sum(command[1], command[2]);
+        break;
+      case 2:
+        return this.sub(command[1], command[2])
+        break;
+      case 3:
+        return this.sqt(command[1], command[2])
+        break;
+      default:
+
+    }
+  }
+  this.sum = function(number1, number2){
+    return number1 + number2
+  }
+    this.sqt = function(number1, number2){
+    return number1 * number2
+  }
+    this.sub = function(number1, number2){
+    return number1 - number2
+  }
+}
---
+++ app/scripts/main.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function(){
+  $("#btnTraduzir").click(function(){
+    var textoAssembly = $("#txtAreaAssembly").val()
+    if (textoAssembly.trim() == "") { $("#txtAreaBinary").val("");  return }
+
+    var programa = textoAssembly.split("\n");
+    $.each(programa, function(index, linha){
+      var command =  linha.trim().split(" ");
+      $.each(command, function(campo, texto){
+        command[campo] = toBinaryFunction(texto);
+      });
+      programa[index] = command.join("");
+    });
+    $("#txtAreaBinary").val(programa.join("\n"));
+
+
+  });
+
+  $("#btnExecutar").click(function(){
+
+    var programa = $("#txtAreaBinary").val().split("\n");
+    var computador = new Computador();
+    computador.processador.start(programa);
+
+
+  });
+
+});
+
+
+function toBinaryFunction (command){
+  switch (command) {
+    case "sum":
+      return "1".padleft(4, 0);
+      break;
+    case "sub":
+      return "10".padleft(4, 0);
+    case "sqt":
+      return "11".padleft(4, 0);
+      break;
+    case "jmp":
+      return "100".padleft(4, 0).padright(18, 0);
+      default:
+      return parseInt(command).toString(2).padleft(14, 0);
+      break;
+  }
+}
+
+
+String.prototype.padleft = function (number, caracter) {
+  var str = this;
+  while (str.length < number) {
+    str = caracter + "" + str
+  }
+  return str;
+};
+
+
+String.prototype.padright = function (number, caracter) {
+  var str = this;
+  while (str.length < number) {
+    str += caracter
+  }
+  return str;
+};
